0fc3089b1e05ef836f5088f47805b87772b69ef4
monidipa1289 <90109557+monidipa1289@users.noreply.github.com>
2022-07-10 15:35:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,18 @@
-let div = document.querySelector('div');
-let button = document.querySelectorAll('button');
-
-let expression="";
-for(b of button)
-{
-    b.addEventListener('click',function(e){
-        if(e.target.innerText==='=')
-        {
-            expression = expression.replace("^", "**");
-            expression=eval(expression);
-            // expression=parseFloat(expression);
-        }
-        else if(e.target.innerText==='DEL')
-        {
-            expression=expression.slice(0,expression.length-1);
-        }
-        else if(e.target.innerText==='AC')
-        {
-           expression="";
-        }
-        else
-        {
-         expression+=e.target.innerText;
+        const hour = document.querySelector('.hour');
+        const minutes = document.querySelector('.minute');
+        const seconds = document.querySelector('.seconds');
+        const digital = document.querySelector('.digital');
+        const container = document.querySelector('.container');
+        setInterval(() => {
+             let h = new Date().getHours();
+             let m = new Date().getMinutes();
+             let s = new Date().getSeconds();
+             if(h>=12)
+             h=h-12;
+             seconds.style.transform = `rotate(${s*6}deg)`;
+             minutes.style.transform = `rotate(${6*m+0.1*s}deg)` ;
+             hour.style.transform = `rotate(${30*h+0.5*m+0.00833*s}deg)` ;
+             digital.innerHTML = `${h} : ${m} : ${s}`;
+        }, 1000);
         
-        }
-        div.innerText=expression;
-    })
-}
-
-
+    
